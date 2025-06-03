c83678da2720524cb5a83317ee2a786f7c366088
penguin104 <jd1908_0808@yahoo.co.jp>
2025-06-03 22:34:44 +0900

    first commit

---
+++ lib/src/home.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+
+class HomeWidget extends StatefulWidget {
+  const HomeWidget({super.key});
+
+  @override
+  State<HomeWidget> createState() => _HomeWidgetState();
+}
+
+class _HomeWidgetState extends State<HomeWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Center(
+          child: Text(
+            "ポケットペンギン図鑑",
+            textAlign: TextAlign.center,
+            style: TextStyle(fontSize: 25),
+          ),
+        ),
+        leading: IconButton(
+          onPressed: () {
+            print("show lists");
+          },
+          icon: Icon(Icons.list, size: 50),
+        ),
+        backgroundColor: Colors.grey[700],
+        foregroundColor: Colors.white,
+        toolbarHeight: 100,
+      ),
+      body: MainContent(),
+    );
+  }
+}
+
+class MainContent extends StatelessWidget {
+  const MainContent({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Column(
+        children: [
+          //contents
+        ],
+      ),
+    );
+  }
+}
